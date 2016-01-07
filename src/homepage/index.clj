782e676b9878ab41a6fdb56116ebfe0bc07993bf
Ayaz Maroof & Laura Sempere <laura+ayaz@exiconglobal.com>
2016-01-07 17:06:43 +0800

    [#110485090] Layout updates

--- src/homepage/index.clj
+++ src/homepage/index.clj
@@ -1,43 +1,43 @@
 (ns homepage.index
-  (:use
-    [hiccup.core :only (html)]
-    [hiccup.page :only (html5 include-css include-js)])
-  (:require
-    [clojure.string :as s]
-    [homepage.integrations :refer
-     [google-analytics hubspot-analytics]]))
+    (:use
+      [hiccup.core :only (html)]
+      [hiccup.page :only (html5 include-css include-js)])
+    (:require
+      [clojure.string :as s]
+      [homepage.integrations :refer
+       [google-analytics hubspot-analytics]]))
 
 (def intro
-  {:header  "The complexity of mobile made easy"
-   :sub-header "Mobile Workflow Management (what!)"})
+  {:header     "The complexity of mobile made easy"
+   :sub-header "Mobile Workflow Management"})
 
 (def overview
-  {:fe [{:header "Taking your organization mobile?"
-         :link "https://www.exiconglobal.com/appboard"}
-        {:header "Want more impact from your apps?"
-         :link "https://www.exiconglobal.com/appboard"}
-        {:header "Not sure what to build? Or who should build it?"
-         :link "https://www.exiconglobal.com/appbuilder"}]
+  {:fe     [{:header "Not sure what to build? Or who should build it?"
+             :link   "https://www.exiconglobal.com/appbuilder"}
+            {:header "Taking your organization mobile?"
+             :link   "https://www.exiconglobal.com/appboard"}
+            {:header "Want more impact from your apps?"
+             :link   "https://www.exiconglobal.com/appboard"}]
    :header "The AppBoard"
-   :le [{:header "Less Hassle"
-         :link "https://www.exiconglobal.com/appboard"
-         :text "Secure, dashboard with tools, analytics +
-               resources for your team"}
-        {:header "More Power"
-         :link "https://www.exiconglobal.com/appboard"
-         :text "Business intelligence to make smarter
-               decisions and improved ROI"}
-        {:header "Better Apps"
-         :link "https://www.exiconglobal.com/appbuilder"
-         :text "Be guided through defining an app + get matched with
-               3 great development companies"}]})
 
+   :le     [{:header "Less Hassle"
+             :text   "Secure, dashboard with tools, analytics +
+               resources for your team"
+             :link   "https://www.exiconglobal.com/appboard"}
+            {:header "More Power"
+             :text   "Business intelligence to make smarter
+               decisions and improved ROI"
+             :link   "https://www.exiconglobal.com/appboard"}
+            {:header "Better Apps"
+             :text   "Be guided through defining an app + get matched with
+               3 great development companies"
+             :link   "https://www.exiconglobal.com/appbuilder"}]})
 
 (def testimonials
-  [{:photo "/img/testimonials/richard-sofer.png"
-    :logo "/img/testimonials/every-hotels.jpg"
-    :name "Richard Sofer"
-    :title "Brand CEO, every hotels"
+  [{:photo       "/img/testimonials/richard-sofer.png"
+    :logo        "/img/testimonials/every-hotels.jpg"
+    :name        "Richard Sofer"
+    :title       "Brand CEO, every hotels"
     :testimonial "I give them credit for teaching me everything
                  I know about APIs! It has been a great partnership,
                  they are smart, responsive and the access to apps
@@ -49,76 +49,76 @@
                  and excellent resource for up to date thinking
                  and industry insight."}
 
-   {:photo "/img/testimonials/christopher-david.png"
-    :logo "/img/testimonials/schneider-electric.png"
-    :name "Christopher David"
-    :title "CTO Digital Customer Experience & SVP Software, Schneider Electric"
+   {:photo       "/img/testimonials/christopher-david.png"
+    :logo        "/img/testimonials/schneider-electric.png"
+    :name        "Christopher David"
+    :title       "CTO Digital Customer Experience & SVP Software,
+           Schneider Electric"
     :testimonial "Managing developers was crucial to our business.
-                 Exicon helped us build the foundation on which to
-                 drive our new products, bringing new developers
-                 into our prefered developer programs in over
-                 20 countries which helped us generate a whole
-                 suite of local applications for the different
-                 markets."}
+                        Exicon helped us build the foundation on which to
+                        drive our new products, bringing new developers
+                        into our prefered developer programs in over
+                        20 countries which helped us generate a whole
+                        suite of local applications for the different
+                        markets."}
 
-   {:photo "/img/testimonials/aran-dadswell.png"
-    :logo "/img/testimonials/get-to-know-your-brain.png"
-    :name "Aran Dadswell"
-    :title "Founder of Get to know your Brain"
+   {:photo       "/img/testimonials/aran-dadswell.png"
+    :logo        "/img/testimonials/get-to-know-your-brain.png"
+    :name        "Aran Dadswell"
+    :title       "Founder of Get to know your Brain"
     :testimonial "Wow, great service - thanks! Thanks a million!
                  Thanks for your support so far - we're
                  getting close now!"}])
 
 (def appboard
   {:header "Command Center for your app(s)"
-   :img "/img/landing-page/app-store-analytics.png"
-   :cta ""
-   :text "Business intelligence and tools to help your team take
-         action on your app(s) portfolio"})
-
+   :text   "Business intelligence and tools to help your team take
+         action on your app(s) portfolio"
+   :img    "/img/landing-page/app-store-analytics.png"
+   :cta    "https://www.exiconglobal.com/pricing/"})
 (def customers
   {:header "Our Customer portfolio speaks for itself"
-   :img "/img/landing-page/customers.png"})
+   :img    "/img/landing-page/customers.png"})
 
 (def footer
   [{:header "Company"
-    :items [{:text "About Us"
-             :link "https://www.exiconglobal.com/about-us/"}
-            {:text "Customers"
-             :link "https://www.exiconglobal.com/customers/"}
-            {:text "Media & Press"
-             :link "https://www.exiconglobal.com/news/"}
-            ]}
+    :items  [{:text "About Us"
+              :link "https://www.exiconglobal.com/about-us/"}
+             {:text "Customers"
+              :link "https://www.exiconglobal.com/customers/"}
+             {:text "Media & Press"
+              :link "https://www.exiconglobal.com/news/"}
+             ]}
    {:header "Products"
-    :items [{:text "ApiAxle"
-             :link "http://apiaxle.com/"}
-            {:text "AppBuilder"
-             :link "https://www.exiconglobal.com/appbuilder/"}
-            {:text "AppBoard"
-             :link "https://www.exiconglobal.com/appboard/"}
-            ]}
+    :items  [{:text "ApiAxle"
+              :link "http://apiaxle.com/"}
+             {:text "AppBuilder"
+              :link "https://www.exiconglobal.com/appbuilder/"}
+             {:text "AppBoard"
+              :link "https://www.exiconglobal.com/appboard/"}
+             ]}
    {:header "Developers"
-    :link "https://www.exiconglobal.com/developers/"}
+    :link   "https://www.exiconglobal.com/developers/"}
    {:header "Legal"
-    :items [{:text "Privacy"
-             :link "https://www.exiconglobal.com/privacy-policy/"}
-            {:text "Terms & Conditions"
-             :link "https://www.exiconglobal.com/terms-of-use/"}]}
+    :items  [{:text "Privacy"
+              :link "https://www.exiconglobal.com/privacy-policy/"}
+             {:text "Terms & Conditions"
+              :link "https://www.exiconglobal.com/terms-of-use/"}]}
    {:header "Follow Us"
-    :items [{:image "/img/footer/white-g.png"
-             :link "https://plus.google.com/115123733707845359729/posts"}
-            {:image "/img/footer/white-fb.png"
-             :link "https://www.facebook.com/exicon.mobi"}
-            {:image "/img/footer/white-twitter.png"
-             :link "https://twitter.com/exicon"}
-            {:image "/img/footer/white-linkedin.png"
-             :link "http://www.linkedin.com/company/exicon-ltd-"}
-            {:image "/img/footer/white-youtube.png"
-             :link "https://www.youtube.com/user/Exicon1"}
-            {:image "/img/footer/white-weibo.png"
-             :link "http://www.weibo.com/u/3147916252"}
-            {:image "/img/footer/white-pinterest.png"
-             :link "http://www.pinterest.com/exicon/pins/follow/?guid=CqzN7JCYsQQy-0"}]}])
+    :items  [{:image "/img/footer/white-g.png"
+              :link  "https://plus.google.com/115123733707845359729/posts"}
+             {:image "/img/footer/white-fb.png"
+              :link  "https://www.facebook.com/exicon.mobi"}
+             {:image "/img/footer/white-twitter.png"
+              :link  "https://twitter.com/exicon"}
+             {:image "/img/footer/white-linkedin.png"
+              :link  "http://www.linkedin.com/company/exicon-ltd-"}
+             {:image "/img/footer/white-youtube.png"
+              :link  "https://www.youtube.com/user/Exicon1"}
+             {:image "/img/footer/white-weibo.png"
+              :link  "http://www.weibo.com/u/3147916252"}
+             {:image "/img/footer/white-pinterest.png"
+              :link  "http://www.pinterest.com/exicon/pins/follow/?guid=CqzN7JCYsQQy-0"}]}])
 
 (def top-nav
   [{:text "Calculator"
@@ -137,107 +137,115 @@
     :link "https://www.exiconglobal.com/about-us/"}])
 
 (defn render [{global-meta :meta posts :entries}]
-  (html5
-    {:lang "en"}
-    [:head
-     [:meta {:charset "utf-8"}]
-     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-     [:meta {:itemprop "author" :name "author" :content "Exicon (admin@exiconglobal.com)"}]
-     [:meta {:name "keywords" :itemprop "keywords"
-             :content "Manage Apps, Exicon, App Management, Mobile Relationship Management,
+      (html5
+        {:lang "en"}
+        [:head
+         [:meta {:charset "utf-8"}]
+         [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+         [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+         [:meta {:itemprop "author" :name "author" :content "Exicon (admin@exiconglobal.com)"}]
+         [:meta {:name    "keywords" :itemprop "keywords"
+                 :content "Manage Apps, Exicon, App Management, Mobile Relationship Management,
                       What Is Mobile Relationship Management, MRM, AppBoard,
                       Find The Right Developer, Mobile App Strategy, Do I Need Mobile
                       Analytics, Best Tool for Mobile App Analytics, Digital
                       Asset Management"}]
-     [:meta {:name "description" :itemprop "description"
-             :content "Exicon AppBoard Is An App Management Platform That Helps
+         [:meta {:name    "description" :itemprop "description"
+                 :content "Exicon AppBoard Is An App Management Platform That Helps
                       SMEs & Enterprises Build, Manage & Promote Their Mobile Apps."}]
-     [:title "Manage Your Apps | Exicon | Mobile Relationship Management | AppBoard"]
-     [:link {:rel "shortcut icon" :href "/img/favicon.ico"}]
-     (include-css "/css/site.css")
-     (google-analytics)
-     (hubspot-analytics)]
+                 [:title "Manage Your Apps | Exicon | Mobile Relationship Management | AppBoard"]
+                 [:link {:rel "shortcut icon" :href "/img/favicon.ico"}]
+                 (include-css "/css/site.css")
+                 (google-analytics)
+                 (hubspot-analytics)]
 
-    [:body
-     [:div#top-nav
-      [:img.item {:src "/img/exicon-logo.png"}]
-      [:div.item
-       (for [item top-nav]
-         [:a {:href (:link item)}
-          (:text item)])]
-      [:div.item
-       [:a.button {:href "https://www.exiconglobal.com/pricing/"} "Sign Up"]
-       [:a {:href "https://app.exiconglobal.com/"} "Login"]]]
+           [:body
+            [:div#top-nav
+             [:img.item {:src "/img/exicon-logo.png"}]
+             [:div.item
+              (for [item top-nav]
+                   [:a {:href (:link item)}
+                    (:text item)])]
+             [:div.item
+              [:a.button {:href "https://www.exiconglobal.com/pricing/"} "Sign Up"]
+              [:a {:href "https://app.exiconglobal.com/"} "Login"]]]
 
-     [:div.main
-      [:div#intro
-       [:div.container.column.centered
-        [:h1 (s/upper-case (:header intro))]
-        [:iframe.embed-video {:width "460" :height "215" :frameborder "0"
-                              :src "https://www.youtube.com/embed/ajPA6AhUilI"
-                              :allowfullscreen true}]
-        [:h2 (:sub-header intro)]]]
-
-      [:div#overview
-       [:div.container.row
-        (for [item (:fe overview)]
-          [:div.item (:header item)
-           [:br]
-           [:a {:href (:link item)}
-            "Find out more.."]])]
-       [:div.container.divider]
-       [:div.container.row.centered
-        [:h2 (:header overview)]]
-       [:div.container.row
-        (for [item (:le overview)]
-          [:div.item
-           [:h3.centered (:header item)]
-           [:div (:text item)]
-           [:a {:href (:link item)}
-            "Find out more.."]])]]
-
-      [:div#testimonials
-       [:div.container.row
-        (for [item testimonials]
-          [:div.item.segment
-           [:img {:src (:photo item)}]
-           [:img.right-img {:src (:logo item)}]
-           [:div [:b (:name item)]]
-           [:div (:title item)]
-           [:p (:testimonial item)]
-           ])]]
-
-      [:div#appboard
-       [:div.container.column.centered
-        [:div
-         [:h2 (:header appboard)]
-         [:h3 (:text appboard)]]
-        [:div.container.centered
-         [:img.image {:src (:img appboard)}]]
-        [:div.container.centered
-         [:a.button {:href (:cta appboard)}
-          "Get started now"]]]]
-
-      [:div.container.divider]
-
-      [:div#customers
-       [:div.container.column.centered
-        [:h2 (:header customers)]
-        [:div.container.centered
-         [:img.image {:src (:img customers)}]]]]
-
-      [:div#footer
-       [:div.container.row
-        (for [elem footer]
-          [:div.item
-           [:div [:b (:header elem)]]
-           (for [item (:items elem)]
-             (if (:image item)
-               [:a {:href (:link item)
-                    :target "_blank"}
-                [:img {:src (:image item)}]]
+            [:div.main
+             [:div#intro
+              [:div.container.column.centered
                [:div
-                [:a {:href (:link item)}
-                 (:text item)]]))])]]] ]))
+                [:h1 (s/upper-case (:header intro))]
+                [:h2 (:sub-header intro)]]
+               [:iframe.embed-video {:width           "460" :height "215" :frameborder "0"
+                                     :src             "https://www.youtube.com/embed/ajPA6AhUilI"
+                                     :allowfullscreen true}]]]
+
+             [:section#overview
+              [:div.container.centered.row
+               (for [item (:fe overview)]
+                    [:div.item (:header item)
+                     [:br]
+                     [:a {:href (:link item)}
+                      "Find out more.."]])]]
+
+             [:div.container.divider]
+
+             [:section#appboard
+              [:div.container.column.centered
+               [:div
+                [:h2 (:header appboard)]
+                [:h3 (:text appboard)]]
+               [:div.container.centered
+                [:div.item
+                 [:img.image {:src (:img appboard)}]]]
+               [:div.container.centered
+                [:div.item
+                 [:a.button {:href (:cta appboard)}
+                  "Get started now"]]]]]
+
+             [:div.container.divider]
+
+             [:section
+              [:div.container.row.centered
+               [:h2 (:header overview)]]
+              [:div.container.row
+               (for [item (:le overview)]
+                    [:div.item
+                     [:h2.centered (:header item)]
+                     [:div (:text item)]
+                     [:a {:href (:link item)}
+                      "Find out more.."]])]]
+
+             [:section#testimonials
+              [:div.container.row
+               (for [item testimonials]
+                    [:div.item.segment
+                     [:img {:src (:photo item)}]
+                     [:img.right-img {:src (:logo item)}]
+                     [:div [:b (:name item)]]
+                     [:div (:title item)]
+                     [:p
+                      [:i (:testimonial item)]]
+                     ])]]
+
+             [:section#customers
+              [:div.container.column.centered
+               [:h2 (:header customers)]
+               [:div.container.centered
+                [:div.item
+                 [:img.image {:src (:img customers)}]]]]]
+
+             [:div#footer
+              [:div.container.row
+               (for [elem footer]
+                    [:div.item
+                     [:div [:b (:header elem)]]
+                     (for [item (:items elem)]
+                          (if (:image item)
+                            [:a {:href   (:link item)
+                                 :target "_blank"}
+                             [:img {:src (:image item)}]]
+                            [:div
+                             [:a {:href (:link item)}
+                              (:text item)]]))])]]]]))
 
--- src/homepage/styles.clj
+++ src/homepage/styles.clj
@@ -1,11 +1,13 @@
 (ns homepage.styles
-  (:require
-    [garden.def :refer [defstyles]]
-    [garden.units :refer [px percent s em vmax vh vw]]
-    [garden.color :as color :refer [rgb as-hex]]
-    [garden.stylesheet :refer [at-media]]))
+    (:require
+      [garden.def :refer [defstyles]]
+      [garden.units :refer [px percent s em vmax vh vw]]
+      [garden.color :as color :refer [rgb as-hex]]
+      [garden.stylesheet :refer [at-media]]))
 
-(def big-screen {:min-width (px 720)})
+(def large-screen {:min-width (px 1200)})
+(def big-screen {:min-width (px 720)
+                 :max-width (px 1200)})
 (def medium-screen {:min-width (px 480)
                     :max-width (px 720)})
 (def small-screen {:max-width (px 480)})
@@ -15,114 +17,131 @@
 (def dark-grey (as-hex "#808080"))
 
 (def centered {:justify-content "center"
-               :align-items "center"
-               :text-align "center"})
+               :align-items     "center"
+               :text-align      "center"})
 
 (def top-space (px 68))
 
 (defstyles base
-  [:#top-nav
-   {:position "fixed"
-    :background "white"
-    :width (percent 100)
-    :height top-space
-    :display "flex"
-    :align-items "center"
-    :justify-content "space-around"
-    :box-shadow "0 1px 2px 0 rgba(34,36,38,.15)"}
-   [:a {:margin "0 1em"
-        :color "black"
-        :font-size (em 0.8)}]
-   [:img {:width (px 110)}]
-   [:.item {:flex-shrink 1}]]
+           [:section
+            {:margin  "1em auto"}]
+           [:#top-nav
+            {:position        "fixed"
+             :background      "white"
+             :width           (percent 100)
+             :height          top-space
+             :display         "flex"
+             :align-items     "center"
+             :justify-content "space-around"
+             :box-shadow      "0 1px 2px 0 rgba(34,36,38,.15)"}
+            [:a {:margin    "0 1em"
+                 :color     "black"
+                 :font-size (em 0.8)}]
+            [:img {:width (px 110)}]
+            [:.item {:flex-shrink 1}]]
 
-  [:.main
-   {:padding-top top-space}]
-  [:body
-   {:margin "0"
-    :font-family "Helvetica-Neue, Helvetica, sans-serif"
-    :font-size (px 16)
-    :overflow-x "hidden"}]
-  [:h2 {:font-weight "400"}]
-  [:a {:text-decoration "none"
-       :color teal
-       :font-size (em 0.8)}]
-  [:.container
-   {:display
-    #{"-webkit-box""-moz-box""-ms-flexbox"
-      "-webkit-flex""flex"}
-    :justify-content "space-between"
-    :margin "1em auto"}
-   [:.item
-    {:padding (em 1)
-     :margin "1em 0.5em"
-     :flex-grow 1}]]
-  [:.column
-   {:flex-flow "column wrap"}]
-  [:.row
-   {:flex-flow "row wrap"}]
-  [:.play-btn {:width (px 45)
-               :margin "0 auto"}]
-  [:.centered
-   {:justify-content "center"
-    :align-items "center"
-    :text-align "center"}]
+           [:.main
+            {:padding-top top-space}]
+           [:body
+            {:margin      "0"
+             :font-family "Helvetica-Neue, Helvetica, sans-serif"
+             :font-size   (px 16)
+             :overflow-x  "hidden"}]
+           [:h2 {:font-weight "400"}]
+           [:a {:text-decoration "none"
+                :color           teal
+                :font-size       (em 0.8)}]
+           [:.container
+            {:display
+                              #{"-webkit-box" "-moz-box" "-ms-flexbox"
+                                "-webkit-flex" "flex"}
+             :justify-content "space-between"
+             :margin          "0 auto"}
+            [:.item
+             {:margin    "1em 0.5em"
+              :flex-grow 1}]]
+           [:.column
+            {:flex-flow "column wrap"}]
+           [:.row
+            {:flex-flow "row wrap"}]
+           [:.play-btn {:width  (px 45)
+                        :margin "0 auto"}]
+           [:.centered
+            {:justify-content "center"
+             :align-items     "center"
+             :text-align      "center"}]
 
-  [:#intro {:display "flex"
-            :background
-            "url('/img/landing-page/header.jpg') center no-repeat"
-            :background-position-y 0
-            :background-size (vmax 280)}
-   [:.embed-video
-    {:width (vmax 7)
-     :height (px 111)
-     :min-width (px 200)}]]
+           [:#intro {:display               "flex"
+                     :background
+                                            "url('/img/landing-page/header.jpg') center no-repeat"
+                     :background-position-y 0
+                     :background-size       (vmax 280)
+                     :padding-bottom (em 0.5)}
+            [:.embed-video
+             {:width     (vmax 7)
+              :height    (px 111)
+              :min-width (px 200)}]]
 
-  [:.right-img
-   {:vertical-align "top"
-    :float "right"}]
-  [:.segment
-   {:background "white"
-    :padding (em 1)
-    :border-radius (em 0.3)
-    :border "1px solid rgba(34,36,38,.15)"}]
+           [:.right-img
+            {:vertical-align "top"
+             :float          "right"}]
+           [:.segment
+            {:background    "white"
+             :padding       (em 1)
+             :border-radius (em 0.3)
+             :border        "1px solid rgba(34,36,38,.15)"}]
 
-  [:#testimonials
-   {:background grey}]
-  [:img.image
-   {:width (vw 90)
-    :max-width (px 900)}]
-  [:.button
-   {:background teal
-    :color "white !important"
-    :cursor "pointer"
-    :padding "0.7em 1em"
-    :border-radius (em 0.3)
-    :font-size (em 1.1)}
-   [:.bordered
-    {:border-radius (em 0.3)
-     :border "solid 1px rgba(34,36,38,.15)"}]]
+           [:#testimonials
+            {:background grey
+             :font-size (em 0.8)
+             :line-height (em 1.4)}]
+           [:img.image
+            {:width     (vw 90)
+             :max-width (px 900)
+             }]
+           [:#appboard
+            [:.image {:box-shadow "0px 2px 2px 0px rgba(34, 36, 38, 0.25)"}]]
+           [:.button
+            {:background    teal
+             :color         "white !important"
+             :cursor        "pointer"
+             :padding       "0.7em 1em"
+             :border-radius (em 0.3)
+             :font-size     (em 1.1)}
+            [:.bordered
+             {:border-radius (em 0.3)
+              :border        "solid 1px rgba(34,36,38,.15)"}]]
 
-  [:#footer
-   {:background dark-grey
-    :color "white"}
-   [:a {:color "white"}]
-   [:.container
-    {:margin-bottom 0}]]
-  [:.divider
-   {:border-top "1px dotted rgba(34,36,38,.15)"
-    :border-bottom "1px dotted rgba(255,255,255,.1)"}]
+           [:#footer
+            {:background dark-grey
+             :color      "white"
+             :padding-bottom (em 2)}
+            [:a {:color "white"}]
+            [:.container
+             {:margin-bottom 0}]]
+           [:.divider
+            {:border-top    "1px dotted rgba(34,36,38,.15)"
+             :border-bottom "1px dotted rgba(255,255,255,.1)"}]
 
-  (at-media big-screen [[:body {:font-size (px 18)}]
-                        [:.container {:width (percent 80)}]
-                        [:#intro {:background-size (percent 100)
-                                  :min-height (vmax 30)
-                                  :font-size (vmax 1.5)}]
-                        [:.container
-                         [:.item {:flex-grow 1
-                                  :flex-basis 0}]]])
-  (at-media medium-screen [[:#top-nav [:a {:font-size (vw 1.5)}]]])
-  (at-media small-screen [])
+           (at-media large-screen [[:body {:font-size (px 19)}]
+                                   [:.container {:width (percent 80)}]
+                                   [:#intro {:background-size (percent 100)
+                                             :min-height      (vmax 30)
+                                             :font-size       (vmax 1.5)}]
+                                   [:.container
+                                    [:.item {:flex-grow  1
+                                             :flex-basis 0}]]
+                                   [:#intro [:.embed-video
+                                             {:width     (px 400)
+                                              :height    (px 224)}]]])
 
-  )
+           (at-media big-screen [[:body {:font-size (px 16)}]
+                                   [:#intro {:background-size (percent 100)
+                                             :min-height      (vmax 30)
+                                             :font-size       (vmax 1.5)}]
+                                   [:.container
+                                    [:.item {:flex-grow  1
+                                             :flex-basis 0}]]])
+           (at-media medium-screen [[:#top-nav [:a {:font-size (vw 1.5)}]]])
+           (at-media small-screen []))
 
