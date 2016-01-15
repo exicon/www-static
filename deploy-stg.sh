a1dea2c1b9c84ec2205c3840bce8127bbce718bf
Tamas Herman <tamas@exiconglobal.com>
2016-01-15 15:33:12 +0800

    Move template to infrastructure

--- deploy-stg.sh
+++ deploy-stg.sh
@@ -4,4 +4,7 @@ WWW_URL=https://www.stg.exicon.mobi \
   NODE_APPBOARD_URL=https://app.stg.exicon.mobi \
   boot build
 
-aws s3 sync --exclude "*.DS_Store" target/public/ s3://stg.app-hq.com/
+# only updates www-static files, to also include old homepage updates
+# or to delete previous files that are not required anymore
+# use the script from infrastructure repo
+aws s3 sync --exclude "*.DS_Store" target/public/ s3://stg.exicon.mobi/
